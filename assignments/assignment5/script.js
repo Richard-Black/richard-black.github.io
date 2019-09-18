56588c65c7cc03d0250000004881eba8e47dfddf
Richard-Black <richablck@gmail.com>
2019-09-18 18:08:19 -0400

    class exercise 9/18/19 added

---
+++ assignments/assignment5/script.js
@@ -0,0 +1,20 @@
+﻿"using strict" 
+var userName = prompt("Enter your name");
+var myP1 = document.getElementById("name");
+myP1.innerHTML = `${userName} ordered`;
+var order = prompt("How many coffees would you like to order?");
+var myP2 = document.getElementById("order");
+if (order > 1) {
+    myP2.innerHTML = ` ${order} coffee(s),`;
+}
+else {
+    myP2.innerHTML = ` ${order} coffee,`;
+}
+var tip = prompt("How much would you like to tip?");
+
+function add(order,tip) {
+    return (((((order * 2.50) * 1.07)+(tip*1.00))*100)/100).toFixed(2);
+}
+var total = add(order,tip);
+var myP4 = document.getElementById("total");
+myP4.innerHTML = `Total: $${total} `;
---
+++ in_class/9_18_19/script.js
@@ -0,0 +1,32 @@
+﻿"using strict"
+//console.log("Hello world");
+//const myName = "Richard";
+//myName = "Lucas";
+//console.log(`Hello world ${myName}`);
+
+//gather fruit names
+const fruit1 = document.getElementById("fruit1").textContent;
+const fruit1 = document.getElementById("fruit2").textContent;
+const fruit1 = document.getElementById("fruit3").textContent;
+
+//write to reuslt paragraph
+const resultElem = document.getElementById("result");
+resultElem.textContent = `You like ${fruit1}, ${fruit2}, ${fruit3}`;
+
+
+//console.log(`first fruit ${fruit1}`);
+//fruit1.innerHTML = "What a good day!";
+
+function displayFruits() {
+    //gather fruit names
+    const fruit1 = document.getElementById("fruit1").textContent;
+    const fruit1 = document.getElementById("fruit2").textContent;
+    const fruit1 = document.getElementById("fruit3").textContent;
+
+    //write to reuslt paragraph
+    const resultElem = document.getElementById("result");
+    resultElem.textContent = `You like ${fruit1}, ${fruit2}, ${fruit3}`;
+}
+//get the button
+const btnResult = document.getElementById("btn-results");
+btnResult.onclick = displayFruits;
