7ab54871e497dbd3bdaf61ef5c62ad3d71dd42be
Richard-Black <richablck@gmail.com>
2019-10-02 16:46:37 -0400

    updated assignments

--- in_class/10_2_19/script.js
+++ in_class/10_2_19/script.js
@@ -44,7 +44,7 @@ function displayTimesTables() {
     //insert table after button
     let tableElem = document.createElement("table");
     //tableElem.classList(table); Add an element so it can be edited on CSS
-    //tableElem.id = "table";
+    //tableElem.id = "table"; 
     this.after(tableElem);  //puts after element
 
     //loop through and create rows
