ddaea6b196da66467f39751e29bd97d94e565e0a
Richard-Black <richablck@gmail.com>
2019-10-02 14:12:22 -0400

    updated assignments

---
+++ assignments/assignment_css/script.js
@@ -0,0 +1,11 @@
+﻿"using strict"
+
+window.onload = function () {
+    const hamburger = document.getElementById("hamburger");
+    hamburger.onclick = toggleNav;
+}
+
+function toggleNav(){
+    const navItems = document.getElementById("main-nav");
+    navItems.classList.toggle("hidden");
+}
