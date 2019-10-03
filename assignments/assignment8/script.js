054f602c3c9887e2c76d50708da77d115b3748f7
Richard-Black <richablck@gmail.com>
2019-10-02 22:00:17 -0400

    updated assignments

---
+++ assignments/assignment8/script.js
@@ -0,0 +1,34 @@
+﻿"using strict" 
+
+function sumEven() {
+    let number = document.getElementById("number").value;
+    let result2 = document.getElementById("result2");
+    let sum = 0;
+    for (i = 0; i <= number;i++ ) {
+        if (i % 2 == 0) {
+            sum = i + sum;
+        }
+    }
+    result2.innerHTML = `Sum: ${sum}`;
+}
+
+function findAscii() {
+    let word = document.getElementById("word").value;
+    let result1 = document.getElementById("result1");
+    var totalAscii =""; 
+    for ( i = 0; i < word.length; i++) {
+        totalAscii += word.charCodeAt(i) + `, `;
+
+    }
+            totalAscii = totalAscii.replace(/,\s*$/, "");
+
+            result1.innerHTML = `${totalAscii}`;
+
+}
+
+window.onload = function onLoad() {
+    const btnNum = document.getElementById("btn-num");
+    const btnWord = document.getElementById("btn-word");
+    btnNum.onclick = sumEven;
+    btnWord.onclick = findAscii;
+}
