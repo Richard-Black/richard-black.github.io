8466c6c4b2bb42b9bbb4e3db10249df0379fa172
Richard-Black <richablck@gmail.com>
2019-10-28 14:53:27 -0400

    added class exercise

---
+++ in_class/10_28_19/script.js
@@ -0,0 +1,21 @@
+﻿"using strict"
+
+async function special(){
+    console.log("In Special Beginning");
+    let result = await setTimeout(doStuff, 2000);
+    console.log("In Special Ending");
+}
+
+function doStuff(){
+    console.log("In do stuff"); 
+}
+
+window.onload = function(){
+    this.console.log("Before calling Special");
+    this.special();
+    this.console.log("After calling Special");
+}
+
+
+
+
